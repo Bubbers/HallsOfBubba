65f922dc165f1dcf39ddd418f234c9dd51299698
Johan Nilsson Hansen <Granen@hotmail.com>
2017-04-28 22:52:35 +0200

    Created simple main function that renders a bubba

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,62 @@
 
+#include <Renderer.h>
+#include <cameras/TopDownCamera.h>
+#include <constants.h>
+#include <ResourceManager.h>
+#include <StandardRenderer.h>
+#include <Scene.h>
+#include <Window.h>
+
+Renderer renderer;
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+TopDownCamera *camera;
+std::shared_ptr<Scene> scene;
+
+void idle(float timeSinceStart,float timeSinceLastCall) {
+}
+
+// Called by the window mainloop
+void display(float timeSinceStart,float timeSinceLastCall) {
+    renderer.drawScene(camera, scene.get(), timeSinceStart);
+}
+
+// Called when the window gets resized
+void resize(int newWidth, int newHeight) {
+    renderer.resize(newWidth, newHeight);
+}
+
+void loadWorld() {
+    std::shared_ptr<ShaderProgram> standardShader = ResourceManager::loadAndFetchShaderProgram(SIMPLE_SHADER_NAME, "", "");
+    std::shared_ptr<Mesh> playerMesh = ResourceManager::loadAndFetchMesh("../assets/meshes/bubba.obj");
+
+    std::shared_ptr<GameObject> gameObject = std::make_shared<GameObject>(playerMesh);
+    gameObject->setLocation(chag::make_vector(0.0f, 0.0f, 0.0f));
+    StandardRenderer* stdrenderer = new StandardRenderer(playerMesh, standardShader);
+    gameObject->addRenderComponent(stdrenderer);
+    gameObject->setDynamic(true);
+
+    scene = std::make_shared<Scene>();
+    scene->addShadowCaster(gameObject);
+}
+
 int main() {
+    Window* win = new Window(SCREEN_WIDTH, SCREEN_HEIGHT, "Halls of Bubba");
+
+    // Setup callbacks for window events
+    win->setResizeMethod(resize);
+    win->setIdleMethod(idle);
+    win->setDisplayMethod(display);
+
+    renderer.initRenderer(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    camera = new TopDownCamera(chag::make_vector(0.0f, 0.0f, 0.0f), SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    loadWorld();
+
+    win->start(60);
+
     return 0;
 }
