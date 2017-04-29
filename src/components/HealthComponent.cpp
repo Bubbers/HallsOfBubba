f032bdffd3da84edf794fa4984a09f92029e5a1c
Johan Nilsson Hansen <Granen@hotmail.com>
2017-04-29 11:40:02 +0200

    Healthcomponent now decreases health for any odd identifier

--- src/components/HealthComponent.cpp
+++ src/components/HealthComponent.cpp
@@ -2,7 +2,6 @@
 // Created by simon on 2017-04-29.
 //
 
-#include <ObjectIdentifiers.h>
 #include "HealthComponent.h"
 
 void HealthComponent::update(float dt) {
@@ -10,7 +9,7 @@ void HealthComponent::update(float dt) {
 }
 
 void HealthComponent::beforeCollision(std::shared_ptr<GameObject> collider) {
-    if(collider->getIdentifier() == ENEMY_SPAWNED_BULLET){
+    if(collider->getIdentifier() & 1){
         health--;
     }
     if(health <= 0){
