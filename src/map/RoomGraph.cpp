9aa76e0483c8456d14ab1e6b6909d00220e89a1e
Viktor Sjölind <viktor@sjolind.se>
2017-09-23 21:47:26 +0200

    RoomGraph: Multiple levels + cleanup
    Signed-off-by: Viktor Sjölind <viktor@sjolind.se>

--- src/map/RoomGraph.cpp
+++ src/map/RoomGraph.cpp
@@ -2,72 +2,73 @@
 #include "BossRoom.h"
 #include "HallwayRoom.h"
 
-RoomGraph::RoomGraph(std::function<void(Direction)> walkCallback, std::function<void()> &allPlayersDead) {
+RoomGraph::RoomGraph(std::function<void(Direction)> walkCallback,
+                     std::function<void()> &allPlayersDead)
+{
     generateGraph(walkCallback, allPlayersDead);
 }
 
-std::shared_ptr<Room> RoomGraph::getCurrentRoom(){
-    return graph[currentX][currentY];
+std::shared_ptr<Room> RoomGraph::getCurrentRoom()
+{
+    return graph[currentX][currentY][currentLevel];
 }
 
-void RoomGraph::walk(Direction direction) {
-    std::pair<int, int> next = getNextFromDir(direction);
-    std::shared_ptr<Room> theRoom = graph[next.first][next.second];
-    if(!theRoom){
+void RoomGraph::walk(Direction direction)
+{
+    level_pos_t next = getNextFromDir(direction);
+    if (!graph[next.first][next.second][currentLevel]) {
         throw "Tried to get a room that didn't exist";
     }
+
     currentX = next.first;
     currentY = next.second;
 }
 
-void RoomGraph::addPath(std::shared_ptr<Room> nextRoom, Direction direction) {
-    std::pair<int, int> offset = getNextFromDir(direction);
-    if(!graph[offset.first][offset.second]){
-        throw "You can't assign a direction two different rooms.";
-    }
-    graph[offset.first][offset.second] = nextRoom;
-}
-
-std::pair<int, int> RoomGraph::getNextFromDir(Direction direction){
+level_pos_t RoomGraph::getNextFromDir(Direction direction)
+{
     int nextX = 0, nextY = 0;
-    if(direction == RIGHT){
-        nextX = 1;
-    }else if(direction == LEFT){
-        nextX = -1;
-    }else if(direction == DOWN){
-        nextY = -1;
-    }else if(direction == UP){
-        nextY = 1;
+    switch (direction) {
+        case RIGHT: nextX =  1; break;
+        case LEFT:  nextX = -1; break;
+        case UP:    nextY =  1; break;
+        case DOWN:  nextY = -1; break;
     }
-    return std::pair<int,int>(currentX+nextX, currentY+nextY);
+    return level_pos_t(currentX + nextX, currentY + nextY);
 };
 
-
-void RoomGraph::generateGraph(std::function<void(Direction)> walkCallback, std::function<void()> &allPlayersDead)
+void RoomGraph::generateGraph(
+        std::function<void(Direction)> walkCallback,
+        std::function<void()> &allPlayersDead)
 {
-    auto startRoom    = std::pair<int, int>(3, 0);
+    auto startRoom = level_pos_t(3, 0);
 
     int bossRoomX = rand() % GRAPH_WIDTH;
     int bossRoomY = rand() % GRAPH_HEIGHT;
-    auto bossRoom     = std::pair<int, int>(bossRoomX, bossRoomY);
+    auto bossRoom = level_pos_t(bossRoomX, bossRoomY);
 
     int treasureRoomX = rand() % GRAPH_WIDTH;
     int treasureRoomY = rand() % GRAPH_HEIGHT;
 
-    auto treasureRoom = std::pair<int, int>(treasureRoomX, treasureRoomY);
+    auto treasureRoom = level_pos_t(treasureRoomX, treasureRoomY);
 
-    graph[bossRoom.first][bossRoom.second] = std::make_shared<BossRoom>(allPlayersDead);
-    generatePath(startRoom, treasureRoom, allPlayersDead);
-    generatePath(startRoom, bossRoom, allPlayersDead);
-    generateDoors(walkCallback);
+    graph[bossRoom.first][bossRoom.second][currentLevel] =
+            std::make_shared<BossRoom>(allPlayersDead);
+
+    for (unsigned int level = 0; level < GRAPH_LEVELS; ++level) {
+        generatePath(startRoom, treasureRoom, allPlayersDead, level);
+        generatePath(startRoom, bossRoom, allPlayersDead, level);
+        generateDoors(walkCallback, level);
+    }
 
     currentX = startRoom.first;
     currentY = startRoom.second;
+    currentLevel = 0;
 }
 
-void RoomGraph::generatePath(std::pair<int, int> startRoom,
-                             std::pair<int, int> targetRoom,
-                             std::function<void()> &allPlayersDead)
+void RoomGraph::generatePath(level_pos_t startRoom,
+                             level_pos_t targetRoom,
+                             std::function<void()> &allPlayersDead,
+                             unsigned int level)
 {
     const int xMin = std::min(startRoom.first, targetRoom.first);
     const int xMax = std::max(startRoom.first, targetRoom.first);
@@ -79,8 +80,8 @@ void RoomGraph::generatePath(std::pair<int, int> startRoom,
     for (int y = yMin; y < yMax; ++y) {
         int x = startRoom.first;
 
-        if (!graph[x][y]) {
-            graph[x][y] = std::make_shared<HallwayRoom>(allPlayersDead, false);
+        if (!graph[x][y][level]) {
+            graph[x][y][level] = std::make_shared<HallwayRoom>(allPlayersDead, false);
         }
     }
 
@@ -88,27 +89,30 @@ void RoomGraph::generatePath(std::pair<int, int> startRoom,
     for (int x = xMin; x < xMax; ++x) {
         int y = targetRoom.second;
 
-        if (!graph[x][y]) {
-            graph[x][y] = std::make_shared<HallwayRoom>(allPlayersDead, false);
+        if (!graph[x][y][level]) {
+            graph[x][y][level] = std::make_shared<HallwayRoom>(allPlayersDead, false);
         }
     }
 }
 
-void RoomGraph::generateDoors(std::function<void(Direction)> walkCallback) {
+void RoomGraph::generateDoors(std::function<void(Direction)> walkCallback,
+                              unsigned int level)
+{
     for (int x = 0; x < GRAPH_WIDTH; ++x) {
         for (int y = 0; y < GRAPH_HEIGHT; ++y) {
-            if(graph[x][y]) {
-                if (y + 1 < GRAPH_HEIGHT && graph[x][y + 1]) {
-                    graph[x][y]->addDoor(Direction::UP, walkCallback);
+            auto &room = graph[x][y][level];
+            if (room) {
+                if (y + 1 < GRAPH_HEIGHT && graph[x][y + 1][level]) {
+                    room->addDoor(Direction::UP, walkCallback);
                 }
-                if (y > 0 && graph[x][y - 1]) {
-                    graph[x][y]->addDoor(Direction::DOWN, walkCallback);
+                if (y > 0 && graph[x][y - 1][level]) {
+                    room->addDoor(Direction::DOWN, walkCallback);
                 }
-                if (x + 1 < GRAPH_WIDTH && graph[x + 1][y]) {
-                    graph[x][y]->addDoor(Direction::RIGHT, walkCallback);
+                if (x + 1 < GRAPH_WIDTH && graph[x + 1][y][level]) {
+                    room->addDoor(Direction::RIGHT, walkCallback);
                 }
-                if (x > 0 && graph[x - 1][y]) {
-                    graph[x][y]->addDoor(Direction::LEFT, walkCallback);
+                if (x > 0 && graph[x - 1][y][level]) {
+                    room->addDoor(Direction::LEFT, walkCallback);
                 }
             }
         }
