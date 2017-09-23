cd298aa59bb84cfe1b23e32abcd46618756f8d7a
Viktor Sjölind <viktor@sjolind.se>
2017-09-23 21:47:26 +0200

    RoomGraph: Multiple levels + cleanup
    Signed-off-by: Viktor Sjölind <viktor@sjolind.se>

--- src/map/RoomGraph.h
+++ src/map/RoomGraph.h
@@ -1,9 +1,4 @@
-//
-// Created by simon on 2017-04-30.
-//
-
-#ifndef HALLSOFBUBBA_ROOMGRAPH_H
-#define HALLSOFBUBBA_ROOMGRAPH_H
+#pragma once
 
 #include <map>
 #include <map/Direction.h>
@@ -11,39 +6,36 @@
 
 #define GRAPH_WIDTH 7
 #define GRAPH_HEIGHT 7
+#define GRAPH_LEVELS 3
 
+typedef std::pair<unsigned int, unsigned int> level_pos_t;
 class RoomGraph  {
 
 public:
 
-    RoomGraph(std::function<void(Direction)> walkCallback, std::function<void()> &allPlayersDead);
+    RoomGraph(std::function<void(Direction)> walkCallback,
+              std::function<void()> &allPlayersDead);
+
     ~RoomGraph();
 
     std::shared_ptr<Room> getCurrentRoom();
     void walk(Direction direction);
 
-    void addPath(std::shared_ptr<Room> nextRoom, Direction direction);
-
 private:
 
-    int currentX = 0;
-    int currentY = 0;
-    std::map<Direction, std::weak_ptr<RoomGraph>> paths;
-    std::shared_ptr<Room> graph[GRAPH_WIDTH][GRAPH_HEIGHT] = {{},{},{},{},{},{},{}};
+    unsigned int currentX = 0;
+    unsigned int currentY = 0;
+    unsigned int currentLevel = 0;
 
-    std::pair<int, int> getNextFromDir(Direction direction);
+    std::shared_ptr<Room> graph[GRAPH_WIDTH][GRAPH_HEIGHT][GRAPH_LEVELS];
 
-    bool hasWalked = false;
-    Direction directionWalked;
-
+    level_pos_t getNextFromDir(Direction direction);
 
     void generateGraph(std::function<void(Direction)> walkCallback, std::function<void()> &function);
-    void generatePath(std::pair<int, int> startRoom,
-                      std::pair<int, int> targetRoom,
-                      std::function<void()> &allPlayersDead);
+    void generatePath(level_pos_t startRoom,
+                      level_pos_t targetRoom,
+                      std::function<void()> &allPlayersDead,
+                      unsigned int level);
 
-    void generateDoors(std::function<void(Direction)> walkCallback);
+    void generateDoors(std::function<void(Direction)> walkCallback, unsigned int level);
 };
-
-
-#endif //HALLSOFBUBBA_ROOMGRAPH_H
